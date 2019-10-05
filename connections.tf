749e8eb946fdbed02687c3db162c3742c7e7e581
arash <sarikhani.mehdi@gmail.com>
2019-10-05 22:12:57 +1000

    first terraform command

---
+++ connections.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "ap-southeast-2"
+}
+
+provider "google" {
+    credentials = "${file("../account.json")}"
+    project = "bwstest-1529319727293"
+    region = "us-west1"
+}
+
+provider "azurerm" {
+    subscription_id = "0"
+    client_id = "1"
+    client_secret = "2"
+    tenant_id = "1"
+}
